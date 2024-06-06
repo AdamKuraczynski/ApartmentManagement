60123eb360e419e13de0f843cd9bdb570e2f90ce
MadBoy005 <adamczykm684@gmail.com>
2024-06-07 00:11:52 +0200

    update register.php

--- register.php
+++ register.php
@@ -6,24 +6,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = sanitize_input($_POST["username"]);
     $email = sanitize_input($_POST["email"]);
     $password = password_hash(sanitize_input($_POST["password"]), PASSWORD_BCRYPT);
+    $first_name = sanitize_input($_POST["first_name"]);
+    $last_name = sanitize_input($_POST["last_name"]);
+    $phone_number = sanitize_input($_POST["phone_number"]);
+    $address = sanitize_input($_POST["address"]);
+    $role = sanitize_input($_POST["role"]);
 
-    $sql = "INSERT INTO Users (username, email, password_hash) VALUES ('$username', '$email', '$password')";
-    if ($conn->query($sql) === TRUE) {
-        $user_id = $conn->insert_id;
+    $conn->begin_transaction();
 
-        $sql = "INSERT INTO Tenants (user_id) VALUES ('$user_id')";
-        if ($conn->query($sql) === TRUE) {
-            echo "Registration successful!";
-            header("Location: /apartmentmanagement/login.php");
-            exit();
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    try {
+        $sql = "INSERT INTO Users (username, email, password_hash) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("sss", $username, $email, $password);
+        $stmt->execute();
+        $user_id = $stmt->insert_id;
+
+        $sql = "INSERT INTO UserDetails (user_id, first_name, last_name, phone_number, address) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("issss", $user_id, $first_name, $last_name, $phone_number, $address);
+        $stmt->execute();
+
+        $sql = "INSERT INTO Tenants (user_id) VALUES (?)";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $user_id);
+        $stmt->execute();
+
+        $conn->commit();
+
+        echo "Registration successful!";
+        header("Location: /apartmentmanagement/login.php");
+        exit();
+    } catch (Exception $e) {
+        $conn->rollback();
+        echo "Error: " . $e->getMessage();
     }
-
-    $conn->close();
 }
 ?>
 
@@ -45,6 +62,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         Username: <input type="text" name="username" required><br>
         Email: <input type="email" name="email" required><br>
         Password: <input type="password" name="password" required><br>
+        First Name: <input type="text" name="first_name" required><br>
+        Last Name: <input type="text" name="last_name" required><br>
+        Your Adress: <input type="text" name="address"><br>
+        Phone Number: <input type="text" name="phone_number"><br>
         <input type="submit" value="Register">
     </form>
 
