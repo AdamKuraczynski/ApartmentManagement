3c59b0c70b4e4bbc85a5ba59194bf49ab899b7dc
MadBoy005 <adamczykm684@gmail.com>
2024-06-06 19:07:25 +0200

    viev task rebuild v1

--- dashboards/administrator_dashboard.php
+++ dashboards/administrator_dashboard.php
@@ -20,22 +20,52 @@ if (!isset($_SESSION['user_id']) || !check_user_role($conn, $_SESSION['user_id']
     <?php include('../includes/header.php'); ?>
     <main>
         <h2>Admin Dashboard</h2>
-        <ul>
-            <li><a href="../property/add_property.php">Add Property</a></li>
-            <li><a href="../property/edit_property.php">Edit Property</a></li>
-            <li><a href="../property/view_property.php">View Property</a></li>
-            <li><a href="../rental/add_agreement.php">Add Rental Agreement</a></li>
-            <li><a href="../rental/edit_agreement.php">Edit Rental Agreement</a></li>
-            <li><a href="../rental/view_agreement.php">View Rental Agreements</a></li>
-            <li><a href="../maintenance/add_task.php">Add Maintenance Task</a></li>
-            <li><a href="../maintenance/edit_task.php">Edit Maintenance Task</a></li>
-            <li><a href="../maintenance/view_task.php">View Maintenance Task</a></li>
-            <li><a href="../documents/upload_document.php">Upload Document</a></li>
-            <li><a href="../documents/view_document.php">View Document</a></li>
-            <li><a href="../notifications/view_notifications.php">View Notifications</a></li>
-            <li><a href="../pdf/generate_pdf.php">Generate Report</a></li>
-            <li><a href="../roles/modify_roles.php">Modify Roles</a></li>
-        </ul>
+        <br/>
+        <div class = "admin_dashboard">
+        
+            <h3> Manage all properties </h3>
+            <ul>
+                <li><a href="../property/add_property.php">Add Property</a></li>
+                <li><a href="../property/edit_property.php">Edit Property</a></li>
+                <li><a href="../property/view_property.php">View Property</a></li>
+            </ul>
+            
+            <h3> Manage agreements </h3>
+            <ul>
+                <li><a href="../rental/add_agreement.php">Add Rental Agreement</a></li>
+                <li><a href="../rental/edit_agreement.php">Edit Rental Agreement</a></li>
+                <li><a href="../rental/view_agreement.php">View Rental Agreements</a></li>
+            </ul>
+       
+            <h3> Manage maintenance tasks </h3>
+            <ul>
+                <li><a href="../maintenance/add_task.php">Add Maintenance Task</a></li>
+                <li><a href="../maintenance/edit_task.php">Edit Maintenance Task</a></li>
+                <li><a href="../maintenance/view_task.php">View Maintenance Task</a></li>
+            </ul>   
+       
+            <h3> Manage documents </h3>
+            <ul>
+                <li><a href="../documents/upload_document.php">Upload Document</a></li>
+                <li><a href="../documents/view_document.php">View Document</a></li>
+            </ul>
+       
+            <h3> Manage notifications </h3>
+            <ul>
+                <li><a href="../notifications/view_notifications.php">View Notifications</a></li>
+            </ul>
+       
+            <h3> Manage reports </h3>
+            <ul>
+                <li><a href="../pdf/generate_pdf.php">Generate Report</a></li>
+            </ul>
+
+       
+            <h3> Manage users </h3>
+            <ul>
+                <li><a href="../roles/modify_roles.php">Modify Roles</a></li>
+            </ul>
+        </div>
     </main>
     <?php include('../includes/footer.php'); ?>
 </body>
